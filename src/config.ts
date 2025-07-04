345c393f428c5d335723a887427a2d4d4c9dd71d
Bxgldh <1335943616@qq.com>
2025-07-04 17:30:17 +0800

    第一篇博客

--- src/config.ts
+++ src/config.ts
@@ -83,6 +83,7 @@ export const licenseConfig: LicenseConfig = {
 	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
 };
 
+
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
 	// Please select a dark theme, as this blog theme currently only supports dark background color
